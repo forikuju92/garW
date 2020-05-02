d0a99f13365c8e54491346b4943a33a72422198f
Funmilayo Olasehinde <forikuju92@outlook.com>
2020-05-02 15:53:42 +0100

    Added controllers and pages

--- garWeb/Contollers/app.js
+++ garWeb/Contollers/app.js
@@ -1,1 +1,25 @@
-﻿
+﻿var garApp = angular.module("garApp", [ngRoute]);
+
+garApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'Home.html', controller: 'HomeController'
+        })
+        .when('/whoweare', {
+            templateUrl: 'About.html', controller: 'AboutController'
+        })
+        .when('/Products', {
+            templateUrl: 'Product.html', controller: 'ProductsController'
+        })
+        .when('/services', {
+            templateUrl: 'Service.html', controller: 'ServiceController'
+        })
+        .when('/Contact', {
+            templateUrl: 'Contact.html', controller: 'ContactController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+}]);
